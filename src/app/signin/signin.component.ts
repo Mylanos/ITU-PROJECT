91ad0f046ebf5953b08edb08ddc2e2c6d0b85cbb
Horox <horoxj@gmail.com>
2019-12-09 00:19:31 +0100

    added/changed classes and services

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { User } from './user';
+import { UserService } from './user.service';
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
-export class SigninComponent implements OnInit {
 
-  constructor() { }
+export class SigninComponent implements OnInit {
+  users: User[];
+  error = '';
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
+    console.log('hue hue hue');
+    this.getUsers();
+    console.log(this.users);
   }
 
-  signinUser(event){
+  signinUser() {
     console.log('hahhahahah');
   }
 
+  getUsers(): void {
+    
+  this.userService.getAll().subscribe(
+    (res: User[]) => {
+      this.users = res;
+    },
+    (err) => {
+      this.error = err;
+    }
+  );
+  
+}
 
 }
